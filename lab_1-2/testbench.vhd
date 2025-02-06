685d0a131a137d27eb9448b78cfb1e29ff31213b
Amadeus8182 <amadeus8182@gmail.com>
2025-02-06 02:40:18 -0600

    lab_1-2: new testbench

--- lab_1-2/testbench.vhd
+++ lab_1-2/testbench.vhd
@@ -1,4 +1,3 @@
--- Code your testbench here
 library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.std_logic_unsigned.all;
@@ -41,47 +40,46 @@ begin
     
     StimulusProcess : process
     begin
-    	Reset <= '1';
-        Enable <= '0';
-        wait for ClockPeriod;
-        
-        Reset <= '0';
-        Enable <= '1';
-        wait for ClockPeriod * 15;
-        -- Need to test for 00:00:15 (00:00:0F)
-        assert Seconds = X"0F" report "Test 1/5: Expected Seconds: 0F, Actual: " & to_hstring(Seconds) & "." severity error;
-        assert Minutes = X"00" report "Test 1/5: Expected Minutes: 00, Actual: " & to_hstring(Minutes) & "." severity error;
-        assert Hours   = X"00" report "Test 1/5: Expected Hours: 00, Actual: " & to_hstring(Hours) & "." severity error;
         
         Reset <= '1';
         Enable <= '0';
-        wait for ClockPeriod;
-        -- Need to test for 00:00:00
+        wait  for ClockPeriod;
+        
+        Reset <= '0';
+        Enable <= '1';
+        wait for ClockPeriod * 1800;
+        -- Need to test for 00:30:00
         assert Seconds = X"00" report "Test 2/5: Expected Seconds: 00, Actual: " & to_hstring(Seconds) & "." severity error;
-        assert Minutes = X"00" report "Test 2/5: Expected Minutes: 00, Actual: " & to_hstring(Minutes) & "." severity error;
+        assert Minutes = X"1E" report "Test 2/5: Expected Minutes: 00, Actual: " & to_hstring(Minutes) & "." severity error;
         assert Hours   = X"00" report "Test 2/5: Expected Hours: 00, Actual: " & to_hstring(Hours) & "." severity error;
         
         Reset <= '0';
         Enable <= '1';
-        wait for ClockPeriod * 2835;
-        -- Need to test for 00:47:15 (00:2F:0F)
-        assert Seconds = X"0F" report "Test 3/5: Expected Seconds: 0F, Actual: " & to_hstring(Seconds) & "." severity error;
-        assert Minutes = X"2F" report "Test 3/5: Expected Minutes: 2F, Actual: " & to_hstring(Minutes) & "." severity error;
-        assert Hours   = X"00" report "Test 3/5: Expected Hours: 00, Actual: " & to_hstring(Hours) & "." severity error;
+        wait for ClockPeriod * 1890;
+        -- Need to test for 01:01:30 (00:2F:0F)
+        assert Seconds = X"1E" report "Test 3/5: Expected Seconds: 0F, Actual: " & to_hstring(Seconds) & "." severity error;
+        assert Minutes = X"01" report "Test 3/5: Expected Minutes: 2F, Actual: " & to_hstring(Minutes) & "." severity error;
+        assert Hours   = X"01" report "Test 3/5: Expected Hours: 00, Actual: " & to_hstring(Hours) & "." severity error;
         
-        wait for ClockPeriod * 3600;
-        -- Need to test for 01:47:15 (02:2F:0F)
-        assert Seconds = X"0F" report "Test 4/5: Expected Seconds: 0F, Actual: " & to_hstring(Seconds) & "." severity error;
-        assert Minutes = X"2F" report "Test 4/5: Expected Minutes: 2F, Actual: " & to_hstring(Minutes) & "." severity error;
-        assert Hours   = X"01" report "Test 4/5: Expected Hours: 01, Actual: " & to_hstring(Hours) & "." severity error;
+        Reset <= '1';
+        Enable <= '0';
+        wait for ClockPeriod;
+        
+        Reset <= '0';
+        Enable <= '1';
+        wait for ClockPeriod * 5;
         
         Reset <= '0';
         Enable <= '0';
-        wait for ClockPeriod;
-        -- Need to test for 01:47:15
-        assert Seconds = X"0F" report "Test 5/5: Expected Seconds: 0F, Actual: " & to_hstring(Seconds) & "." severity error;
-        assert Minutes = X"2F" report "Test 5/5: Expected Minutes: 2F, Actual: " & to_hstring(Minutes) & "." severity error;
-        assert Hours   = X"01" report "Test 5/5: Expected Hours: 01, Actual: " & to_hstring(Hours) & "." severity error;
+        wait for ClockPeriod * 5;
+        
+        Reset <= '0';
+        Enable <= '1';
+        wait for ClockPeriod * 2;
+        
+        Reset <= '1';
+        Enable <= '1';
+        wait for ClockPeriod * 50;
         
     	stop;
     end process;
@@ -97,4 +95,3 @@ begin
     );
 
 end Behavioral;
-
