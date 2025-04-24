8b02defaf98742568ee69433923174736805bf97
amadeusdotpng <amadeus8182@gmail.com>
2025-04-23 21:37:01 -0500

    lab_10: fix CPU testbench
    it was incomplete

--- lab_10/CPU_TB.vhd
+++ lab_10/CPU_TB.vhd
@@ -25,31 +25,31 @@ begin
         reset <= '1';
 
         -- ADDI R3, R0, 5
-        instruction <= x"0211"; wait for tick;
+        instruction <= x"4305"; wait for tick;
 
         -- ADDI R4, R0, 2
-        instruction <= x"4325"; wait for tick;
+        instruction <= x"4402"; wait for tick;
 
         -- SLT R11, R3, R4
         instruction <= x"7B34"; wait for tick;
 
         -- SW R3, 0(R0)
-        instruction <= x"1432"; wait for tick;
+        instruction <= x"C300"; wait for tick;
 
         -- SW R4, 4(R0)
-        instruction <= x"554C"; wait for tick;
+        instruction <= x"C404"; wait for tick;
 
         -- ADDI R6, R0, 4
-        instruction <= x"2635"; wait for tick;
+        instruction <= x"6604"; wait for tick;
 
         -- LW R7, 0(R6)
-        instruction <= x"3754"; wait for tick;
+        instruction <= x"8760"; wait for tick;
         
         -- LW R8, 0(R0)
-        instruction <= x"3754"; wait for tick;
+        instruction <= x"8800"; wait for tick;
         
         -- ADD R9, R7, R8
-        instruction <= x"3754"; wait for tick;
+        instruction <= x"0978"; wait for tick;
 
         -- SLT R11, R3, R4
         instruction <= x"7A01"; wait for tick;
